b1ad2656f4e46d2624e97e405f495d39426f1b45
Hiba-alkurd <hibaalkurd@gmail.com>
2020-12-11 22:21:35 +0200

    initial commit

---
+++ scripts/train.js
@@ -0,0 +1,37 @@
+
+class Digit{  
+    constructor() {
+        this.value = 0;
+        this.noise = false;
+        this.pixels = [];
+        generate_pixels();
+    }
+
+    generate_pixels(){
+
+    }
+
+}
+
+class Network{
+
+    constructor() {
+        this.training_set = [];  //of digits
+        generate_training_data();
+
+
+
+    }
+
+    generate_training_data(){
+
+    }
+
+
+    train(){
+
+    }
+
+
+
+}
